c43ecda47d6135ebdc251014a24a28276b3169f1
Márk Szávin <szavinm@yahoo.com>
2020-03-18 18:33:16 +0100

    Add ChartData

--- src/BusinessLogic/ChartData.cs
+++ src/BusinessLogic/ChartData.cs
@@ -1,15 +1,30 @@
 ﻿using DataAccess.Model;
+using LiveCharts.Defaults;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BusinessLogic
 {
     public class ChartData
     {
-        List<Subject> subjects;
-        TestEvaluation subjectEvaluation;
+        public List<ScatterPoint> maleScatterPoints = new List<ScatterPoint>();
+        public List<ScatterPoint> femaleScatterPoints = new List<ScatterPoint>();
 
+        public List<ScatterPoint> GetMaleScatterPoints()
+        {
+            SubjectManager subjectManager = new SubjectManager();
+            IQueryable<Subject> subjects = subjectManager.GetAllSubjects();
+
+            double scatterX = 0;
+            double scatterY = 0;
+            double scatterweight = 0;
+
+
+
+            return maleScatterPoints;
+        }
 
 
 
